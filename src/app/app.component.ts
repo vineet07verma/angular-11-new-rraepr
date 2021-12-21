623efff30e0492ad386152899a8b319936cec3d1
vineet07verma <vineet07verma@gmail.com>
2021-12-21 15:24:44 +0530

    Final Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {
   OnChanges,
   AfterViewInit,
 } from '@angular/core';
-import { AtlasmapProvider, IAtlasmapProviderProps } from '@atlasmap/atlasmap';
+import { AtlasmapProvider, IAtlasmapProviderProps, Atlasmap } from '@atlasmap/atlasmap';
 
 @Component({
   selector: 'my-app',
